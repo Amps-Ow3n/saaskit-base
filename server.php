7a985616f0ca7a32d7fb0c60113709b2597de13f
Amps-Ow3n <ampaireowen234@gmail.com>
2025-11-03 23:26:11 +0100

    Fix 404: SPA routing for Render Free Docker

--- server.php
+++ server.php
@@ -1,15 +1,17 @@
 <?php
 
 /**
- * Laravel server.php for Render Free Docker
+ * Laravel server.php for Render Free Docker SPA routing
  */
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+// Serve the requested resource as-is if it exists in public
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
+// Otherwise, route all requests through index.php
 require_once __DIR__.'/public/index.php';
